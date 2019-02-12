ba7fde496f0b6acee9d3a13b1f37c1e1636b705c
sujay-gavhane <sujay.gavhane@gmail.com>
2019-02-12 22:20:38 +0530

    Added devise gem for authentication

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def full_name
+    self.first_name.to_s + " " + self.last_name.to_s
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations' }
+
+  devise_scope :user do
+    authenticated do
+      root to: 'home#index'
+    end
+    unauthenticated do
+      root to: 'devise/sessions#new'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
